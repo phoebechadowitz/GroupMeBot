adad6711830ce5f8e34918a6d52e74a1409c46c2
Phoebe Chadowitz <pc244@duke.edu>
2024-06-09 21:48:21 -0400

    seconds

--- app/controllers/recievers_controller.rb
+++ app/controllers/recievers_controller.rb
@@ -2,13 +2,10 @@ class RecieversController < ApplicationController
   protect_from_forgery with: :null_session
   require 'faraday'
 
-  $seconds = 3
-
   #curl -X POST -d '{"attachments":"", "avatar_url": "https://i.groupme.com/123456789", "created_at": 1302623328, "group_id": "1234567890", "id": "1234567890", "name": "John", "sender_id": "12345", "sender_type": "user", "source_guid": "GUID", "system": false, "text": "someone send the kings cup rules", "user_id": "1234567890"}' -H 'Content-Type: application/json' http://localhost:3000/index
   def create
     text = params[:text]
     if ((text.downcase.include? "kings cup") || (text.downcase.include? "king cup"))
-      sleep($seconds)
       conn = Faraday.new(url: "https://api.groupme.com")
       post_data = {
         bot_id: "2c56ebec9bc1552a0e6e34f7ac",
@@ -25,6 +22,5 @@ class RecieversController < ApplicationController
         request.body = JSON.generate(post_data)
       end
     end
-    $seconds = $seconds + 0.8
   end
 end
